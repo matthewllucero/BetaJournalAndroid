185452cff984a0aa73c5c02897cf03e89d3c65d2
Matthew Lucero <matthew.lucero92@yahoo.com>
2019-06-18 13:12:27 -0700

    added general layout for user to add entries

---
+++ app/src/main/java/com/example/betajournal/AddEntryFragment.java
@@ -0,0 +1,43 @@
+package com.example.betajournal;
+
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AddEntryFragment extends Fragment {
+
+
+    public AddEntryFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_entry, container, false);
+
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+    }
+
+
+}
--- app/src/main/java/com/example/betajournal/EntriesFragment.java
+++ app/src/main/java/com/example/betajournal/EntriesFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -19,6 +20,7 @@ import android.view.ViewGroup;
  */
 public class EntriesFragment extends Fragment {
 
+    private View mView;
 
     public EntriesFragment() {
         // Required empty public constructor
@@ -31,5 +33,19 @@ public class EntriesFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_entries, container, false);
 
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mView = view;
+
+        view.findViewById(R.id.test).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(mView).navigate(R.id.add_entry_action);
+            }
+        });
     }
 }
