51ae7ca65a0d55db9416e65e51874c190c748737
Matthew Lucero <matthew.lucero92@yahoo.com>
2019-06-18 14:02:00 -0700

    added a DatePickerDialog for user to choose date for each entry

--- app/src/main/java/com/example/betajournal/AddEntryFragment.java
+++ app/src/main/java/com/example/betajournal/AddEntryFragment.java
@@ -1,6 +1,7 @@
 package com.example.betajournal;
 
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -10,13 +11,21 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 
 /**
- * A simple {@link Fragment} subclass.
+ * Class created for the AddEntryFragment Fragment in the Navigation Graph
+ * EntriesFragment -> AddEntryFragment -> ...
  */
 public class AddEntryFragment extends Fragment {
 
+    private TextView chosenDate;
+    private Calendar calendar;
+    private DatePickerDialog dpd;
 
     public AddEntryFragment() {
         // Required empty public constructor
@@ -36,6 +45,33 @@ public class AddEntryFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        //TextView created in fragment_add_entry.xml to display the user's chosen date
+        chosenDate = (TextView) view.findViewById(R.id.date_text_view);
+
+        //OnClickListener() created the the "Set Date" button
+        view.findViewById(R.id.set_date_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                calendar = Calendar.getInstance();
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+                int year = calendar.get(Calendar.YEAR);
+
+                dpd = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int mYear, int mMonth, int mDayOfMonth) {
+
+                        //Grabs the current date chosen by the user and puts it into a string to be set in the chosenDate TextField
+                        String date = mMonth + "/" + mDayOfMonth + "/" + mYear;
+                        chosenDate.setText(date);
+                    }
+                }, year, month, day);
+
+                dpd.show();
+
+            }
+        });
 
     }
 
